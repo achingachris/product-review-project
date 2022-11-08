2e9d13039ee55e10bc21c46f258d88e516b84302
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-08 19:55:46 +0300

    bootstrap

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-import {NavLink} from "react-router-dom"
-const Navbar = () => {
+import {NavLink } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+function Navbar(){
   return (
     <div className='container-fluid'>
       <div className='navbar'>
-        <NavLink to='/'>Home</NavLink>
+
+      <NavLink to='/'>Home</NavLink>
         <NavLink to='/product'>Product</NavLink>
         <NavLink to='/review'>Review</NavLink>
         </div>
