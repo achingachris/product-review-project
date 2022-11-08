f617ceb90db4062437668fae11d006f92f61eaad
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-08 12:37:59 +0300

    dd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,3 +3,4 @@ import {Routes, Routes} from 'react-router-dom';
 import Home from "../Components/Home";
 import Navbar from "../Components/Navbar";
 import Product from "../Components/Product";
+import Review from "../Components/Review";
