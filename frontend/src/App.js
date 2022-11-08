2fde82a2dcbc8ea322a85b8bb5d708a1badd2e8d
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-08 13:11:01 +0300

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,3 +4,17 @@ import Home from "../Components/Home";
 import Navbar from "../Components/Navbar";
 import Product from "../Components/Product";
 import Review from "../Components/Review";
+
+
+function App(){
+  return(
+    <div classname="7">
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route exact path='/product'element={<Product/>} />
+        <Route exact path='/review'element={<Review/>} />
+      </Routes>
+    </div>
+  )
+}
