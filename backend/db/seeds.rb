1f6b9b040363b3e878c3212eade8344c1a9ce484
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-09 17:23:48 +0300

    added products in the array

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,8 +10,13 @@ users = [
     {name: 'Angela Mwaniki', contact: 0707000000, email: 'angela@mail.com'},
     {name: 'Jerry Johns', contact: 0707000000, email: 'jerry@mail.com'}
   ]
-  products=[product_name: "Apple", description: faker::lorem, manufacturer: "Apple", product_image: "https://gadgetsleo.com/product/iphone-14-pro-max-1tb/?utm_source=Google%20Shopping&utm_campaign=Google%20Shopping%20Ads&utm_medium=cpc&utm_term=28795&gclid=EAIaIQobChMIxpy0zaKh-wIVa4xoCR1J2wUDEAQYASABEgLUQPD_BwE"
-
+  products=[
+    {product_name: "iPhone 14 Plus 512GB", description: faker::lorem, manufacturer: "Apple", product_image: "https://gadgetsleo.com/product/iphone-14-plus-512gb/"}
+    {product_name: "Nokia G11 Plus", description: faker::lorem, manufacturer: "Nokia", product_image: "https://gadgetsleo.com/product/nokia-g11-plus/"}
+    {product_name: "Infinix Zero Ultra 5G", description: faker::lorem, manufacturer: "Infinix", product_image: "https://gadgetsleo.com/product/infinix-zero-ultra-5g/"}
+    {product_name: "Tecno Spark 9 Pro Sports Edition", description: faker::lorem, manufacturer: "Techno", product_image: "https://gadgetsleo.com/product/tecno-spark-9-pro-sports-edition/"}
+    {product_name: "Samsung Galaxy M23 5G", description: faker::lorem, manufacturer: "Samsung", product_image: "https://gadgetsleo.com/product/samsung-galaxy-m23/"}
+    {product_name: "Oppo Reno 8 4G", description: faker::lorem, manufacturer: "Oppo", product_image: "https://gadgetsleo.com/product/oppo-reno-8-4g/"}
   ]
   
   Review.create(comment: Faker::Lorem, user_id: user.id, product_id: product.id)
