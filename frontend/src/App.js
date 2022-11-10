f8661004c569556285cc744a9aebe40840411196
itschrisagain <chris.achinga@student.moringaschool.com>
2022-11-11 00:02:18 +0300

    comment out review page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 import Layout from './layout/Layout'
 import Home from './pages/Home'
 import Product from './pages/Product'
-import Review from './Components/Review'
+// import Review from './Components/Review'
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />}></Route>
         <Route exact path='/products' element={<Product />} />
-        <Route exact path='/reviews' element={<Review />} />
+        {/* <Route exact path='/reviews' element={<Review />} /> */}
       </Routes>
     </Layout>
   )
