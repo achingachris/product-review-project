6e5a7715c160cecc35088116eec0d33b2464ec61
jerryjohns <jerry.johns@student.moringaschool.com>
2022-11-10 15:44:57 +0300

    add

--- frontend/src/Components/Review.js
+++ frontend/src/Components/Review.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState} from "react";
+import AddReview from "./AddReview";
 
 const Review = () => {
-  return (
-    <div>Review</div>
-  )
-}
+   
+  let params = useParams();
+  const[reviewDetails, setReviewDetails] = useState()
+  useEffect(()=>{
+      fetch(`http://localhost:8000/reviews`)
+      .then((res)=> res.json())
+      .then((data) =>{
+          setReviewDetails(data)
+      })
+  },[]);
 
+return(
+  <div><h2>reviews</h2></div>
+)
+}
 export default Review
+
