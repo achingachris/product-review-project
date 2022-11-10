5f6a670c2a32cd6b22f7a496bf3099ba36b091a6
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-11-10 22:33:55 +0300

    add to appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import Layout from './layout/Layout'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login';
 
 import Home from './pages/Home'
 import Product from './pages/Product'
@@ -16,6 +18,7 @@ function App() {
         <Route exact path='/' element={<Home />}></Route>
         <Route exact path='/products' element={<Product />} />
         <Route exact path='/Form' element={<Form/>} />
+        <Route exact path='/Login' element={<Login/>} />
         <Route exact path='/reviews' element={<Review />} />
       </Routes>
     </Layout>
