31df0e82477c6f557e4c797885aede88c567ee2f
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-10 13:04:38 +0300

    changed images to .jpg

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -12,17 +12,17 @@ users = [
   ]
 
 products = [
-    {product_name: "iPhone 14 Plus 512GB", description: Faker::Lorem, manufacturer: "Apple", product_image: "https://gadgetsleo.com/product/iphone-14-plus-512gb/"},
-    {product_name: "Nokia G11 Plus", description: Faker::Lorem, manufacturer: "Nokia", product_image: "https://gadgetsleo.com/product/nokia-g11-plus/"},
-    {product_name: "Infinix Zero Ultra 5G", description: Faker::Lorem, manufacturer: "Infinix", product_image: "https://gadgetsleo.com/product/infinix-zero-ultra-5g/"},
-    {product_name: "Tecno Spark 9 Pro Sports Edition", description: Faker::Lorem, manufacturer: "Techno", product_image: "https://gadgetsleo.com/product/tecno-spark-9-pro-sports-edition/"},
-    {product_name: "Samsung Galaxy M23 5G", description: Faker::Lorem, manufacturer: "Samsung", product_image: "https://gadgetsleo.com/product/samsung-galaxy-m23/"},
-    {product_name: "Oppo Reno 8 4G", description: Faker::Lorem, manufacturer: "Oppo", product_image: "https://gadgetsleo.com/product/oppo-reno-8-4g/"}
+    {product_name: "iPhone 14 Plus 512GB", description: "it is a good product", manufacturer: "Apple", product_image: "https://m.media-amazon.com/images/I/61FZC+6hDFL._AC_SY300_SX300_.jpg"},
+    {product_name: "Nokia G11 Plus", description: "it is a good product", manufacturer: "Nokia", product_image: "https://m.media-amazon.com/images/I/51vf1R1wS9L._AC_SL1080_.jpg"},
+    {product_name: "Infinix Zero Ultra 5G", description: "it is a good product", manufacturer: "Infinix", product_image: "https://m.media-amazon.com/images/I/41n4NQYMGaL._AC_.jpg"},
+    {product_name: "Tecno Spark 9 Pro Sports Edition", description: "it is a good product", manufacturer: "Techno", product_image: "https://m.media-amazon.com/images/I/712nxo9u15L.__AC_SX300_SY300_QL70_FMwebp_.jpg"},
+    {product_name: "Samsung Galaxy M23 5G", description: "it is a good product", manufacturer: "Samsung", product_image: "https://m.media-amazon.com/images/I/61nhOdlKpDL._AC_SL1500_.jpg"},
+    {product_name: "Oppo Reno 8 4G", description: "it is a good product", manufacturer: "Oppo", product_image: "https://m.media-amazon.com/images/I/51FHswQtVXL.__AC_SY300_SX300_QL70_FMwebp_.jpg"}
   ]
   
-  Review.create(comment: Faker::Lorem, user_id: 4, product_id: 3)
+  Review.create(comment: "The product is ok", user_id: 4, product_id: 3)
 
-  Product.create(product_name: "Iphone 13", description: "It's OK", manufacturer:"Apple", product_image:"https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-01.jpg")
+  # Product.create(product_name: "Iphone 13", description: "It's OK", manufacturer:"Apple", product_image:"https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-01.jpg")
 
 #   t.string :product_name
 #   t.string :description
@@ -31,7 +31,7 @@ products = [
 #   t.datetime :updated_at
 #   t.string :product_image
 
-  Review.create(comment: Faker::Lorem, user_id: 2, product_id: 1)
+  Review.create(comment: "The phone is awesome", user_id: 2, product_id: 1)
   #Review.create(comment: Faker::Lorem, user_id: user.id, product_id: product.id)
   
 #   t.string "comment"
