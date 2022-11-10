8e8331aaabc1511101daf898ee28ebaa5ff2500e
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-10 15:04:15 +0300

    changes on the images

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -12,12 +12,12 @@ users = [
   ]
 
 products = [
-    {product_name: "iPhone 14 Plus 512GB", description: "it is a good product", manufacturer: "Apple", product_image: "https://m.media-amazon.com/images/I/61FZC+6hDFL._AC_SY300_SX300_.jpg"},
-    {product_name: "Nokia G11 Plus", description: "it is a good product", manufacturer: "Nokia", product_image: "https://m.media-amazon.com/images/I/51vf1R1wS9L._AC_SL1080_.jpg"},
-    {product_name: "Infinix Zero Ultra 5G", description: "it is a good product", manufacturer: "Infinix", product_image: "https://m.media-amazon.com/images/I/41n4NQYMGaL._AC_.jpg"},
-    {product_name: "Tecno Spark 9 Pro Sports Edition", description: "it is a good product", manufacturer: "Techno", product_image: "https://m.media-amazon.com/images/I/712nxo9u15L.__AC_SX300_SY300_QL70_FMwebp_.jpg"},
-    {product_name: "Samsung Galaxy M23 5G", description: "it is a good product", manufacturer: "Samsung", product_image: "https://m.media-amazon.com/images/I/61nhOdlKpDL._AC_SL1500_.jpg"},
-    {product_name: "Oppo Reno 8 4G", description: "it is a good product", manufacturer: "Oppo", product_image: "https://m.media-amazon.com/images/I/51FHswQtVXL.__AC_SY300_SX300_QL70_FMwebp_.jpg"}
+    {product_name: "iPhone 14 Plus 512GB", description: "it is a good product", manufacturer: "Apple", product_image: "https://images.unsplash.com/photo-1638038772924-ef79cce2426d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80.png"},
+    {product_name: "Nokia G11 Plus", description: "it is a good product", manufacturer: "Nokia", product_image: "https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=367&q=80.png"},
+    {product_name: "Infinix Zero Ultra 5G", description: "it is a good product", manufacturer: "Infinix", product_image: "https://images.unsplash.com/photo-1546054454-aa26e2b734c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80.png"},
+    {product_name: "Tecno Spark 9 Pro Sports Edition", description: "it is a good product", manufacturer: "Techno", product_image: "https://images.unsplash.com/photo-1548094891-c4ba474efd16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=545&q=80.png"},
+    {product_name: "Samsung Galaxy M23 5G", description: "it is a good product", manufacturer: "Samsung", product_image: "https://images.unsplash.com/photo-1597762470488-3877b1f538c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80.png"},
+    {product_name: "Oppo Reno 8 4G", description: "it is a good product", manufacturer: "Oppo", product_image: "https://images.unsplash.com/photo-1558885544-2defc62e2e2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80.png"}
   ]
   
   Review.create(comment: "The product is ok", user_id: 4, product_id: 3)
