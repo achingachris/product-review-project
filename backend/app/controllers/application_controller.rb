69b9c7606f352dc95d9988ed6fa5087a31c5655b
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-11 09:08:22 +0300

    Product POST-create

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -16,5 +16,17 @@ class ApplicationController < Sinatra::Base
         product.to_json(include: :reviews)
       end 
   
+      #POST
+
+      post '/products' do
+        product = Product.create(
+          product_name: params[:product_name],
+          description: params[:description],
+          manufacturer: params[:manufacturer],
+          product_image: params[:product_image]
+        )
+        mechanic.to_json
+      end
+
 
 end
